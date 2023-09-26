194097703b956058b7d912dfaab8c05d7789008d
Kelvin Ha <haleduykhang@gmail.com>
2023-09-26 23:22:19 +0700

    fix startup.sh

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,6 +4,6 @@ if [ -f .devcontainer/setup-mariadb.sql ]; then
   mysql -h 127.0.0.1 -uroot -pmariadb < .devcontainer/setup-mariadb.sql
 fi
 
-if [ -f .devcontainer/H_Plus_Sports_MySQL.sql ]; then
-  mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/H_Plus_Sports_MySQL.sql
+if [ -f .devcontainer/up.sql ]; then
+  mysql -h 127.0.0.1 -umariadb -pmariadb < .devcontainer/up.sql
 fi
